70a8d648924934be4cea48e297fcc8b74c5cf2ad
RoboMWM <robomwm@users.noreply.github.com>
2025-01-13 20:12:43 -0800

    does curl documentation exist?
    When I'm having to use AI to figure this out, I think there's an issue
    with the documentation

--- OutlookNewBlocker.bat
+++ OutlookNewBlocker.bat
@@ -17,7 +17,7 @@ if not exist %AppData%\OutlookNewBlocker mkdir %AppData%\OutlookNewBlocker
 copy "%~dp0AppxManifest.xml" "%AppData%\OutlookNewBlocker" >nul
 if %errorLevel% neq 0 (
     echo AppxManifest.xml not found, attempting to download from GitHub...
-    curl https://raw.githubusercontent.com/RoboMWM/OutlookNewBlocker/refs/heads/master/AppxManifest.xml -O %AppData%\OutlookNewBlocker\AppxManifest.xml
+    curl https://raw.githubusercontent.com/RoboMWM/OutlookNewBlocker/refs/heads/master/AppxManifest.xml -o %AppData%\OutlookNewBlocker\AppxManifest.xml
     if %errorLevel% neq 0 (
         echo.
         echo Error: Failed to create AppxManifest.xml. Download AppxManifest.xml from GitHub and try again.
